656defc299fb99074f4fecf7823996a75575e11f
Akagi201 <akagi201@gmail.com>
2016-08-03 20:52:40 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@
 time=`date '+%Y-%m-%d-%H:%M:%S'`
 version=`git log -1 --oneline`
 strip_version=${version// /-}
-go build -ldflags "-X main.time='${time}' -X main.version='${strip_version}'"
+GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.time='${time}' -X main.version='${strip_version}'" -o gister_mac
+GOOS=linux  GOARCH=amd64 go build -ldflags "-s -w -X main.time='${time}' -X main.version='${strip_version}'" -o gister_linux
